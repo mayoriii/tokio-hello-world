2015f8f0cfc0ff66cf80198d8f30b187efbd5abf
mayoriii <mfahryfadil@gmail.com>
2026-05-28 01:40:28 +0700

    migrate to poem

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use axum::{Router, routing::get};
 use dotenvy::dotenv;
+use poem::{Route, Server, get, handler, listener::TcpListener};
 use std::env;
 
+#[handler]
+fn hello() -> String {
+    format!("hello")
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), std::io::Error> {
     dotenv().ok();
 
     let port = env::var("PORT").unwrap_or("8000".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
-    let app = Router::new().route("/", get(|| async { "Hello, World" }));
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-
-    axum::serve(listener, app).await.unwrap();
+    let app = Route::new().at("/", get(hello));
+    Server::new(TcpListener::bind(&addr)).run(app).await
 }
